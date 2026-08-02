2e3d4a92f8b3443847135cb5e51da5948c514a9c
WaliurR029 <mdwaliur63@gmail.com>
2026-08-02 23:04:51 +0600

    implemented the resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import dayjs from "dayjs";
 import {navIcons, navLinks} from "#constants";
+import useWindowStore from "#store/window.js";
 
 
 const Navbar = () => {
+    const { openWindow } = useWindowStore();
+
     return (
         <nav>
             <div>
@@ -11,8 +14,8 @@ const Navbar = () => {
                 <p className="font-bold">Waliur's Portfolio</p>
 
                 <ul>
-                    {navLinks.map(({id, name}) =>
-                        (<li key={id}>
+                    {navLinks.map(({id, name, type}) =>
+                        (<li key={id} onClick={() => openWindow(type)}>
                             <p>{name}</p>
                         </li>))}
                 </ul>
