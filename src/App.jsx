c073631e57d766a18ec5b98e8dbdb9d252bcac97
Waliur Rahman <mdwaliur63@gmail.com>
2026-08-16 03:29:05 +0600

    add updated home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import {Draggable} from "gsap/Draggable";
 
-import {Navbar, Welcome, Dock} from "#components"
+import {Navbar, Welcome, Dock, Home} from "#components"
 import {Terminal, Safari, Resume, Finder, Txtfile, ImgFile, Contact} from "#windows"
 
 gsap.registerPlugin(Draggable);
@@ -12,6 +12,7 @@ const App = () => {
           <Navbar/>
           <Welcome/>
           <Dock/>
+          <Home/>
 
           <Terminal/>
           <Safari/>
